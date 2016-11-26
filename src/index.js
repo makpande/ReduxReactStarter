df5a6754066ae3c2be196f23d0dc385bf837babd
makpande <mak_pande@yahoo.com>
2016-11-26 17:33:40 -0500

    Simple starter project for all react app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
+//Create a new component. This component should produce some HTML
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDom from 'react-dom';
 
-import App from './components/app';
-import reducers from './reducers';
+const App = () => {
+  return <div>Hi from React app!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//take this component generate HTML and put it on the page (in the DOM)
+ReactDom.render(<App/>, document.querySelector('.container'));
