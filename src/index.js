aac9b04d7da09f738cc677791edf21373e309dd2
makpande <mak_pande@yahoo.com>
2016-11-30 09:30:00 -0500

    almost completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 //Create a new component. This component should produce some HTML
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyB1K18fpBmARfvEBr53B-XymFX-hf78o90';
 
-const App = () => {
-  return <div>Hi from React app!</div>;
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('heartfulness');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render () {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+  return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo}) } videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 //take this component generate HTML and put it on the page (in the DOM)
